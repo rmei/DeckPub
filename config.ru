82fc8f93a172d2aa0595bbbc54bbf89faebbc3c5
Peter Mei <petermei@gmail.com>
2011-08-28 21:19:38 -0700

    andova begarin

---
+++ config.ru
@@ -0,0 +1,2 @@
+require File.join(File.dirname(__FILE__), 'deck_pub')
+run Sinatra::Application
---
+++ deck_pub.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+
+post '/fetch' do # gettin stuffs fro dropbox
+
+=begin
+  commit_json = JSON.parse(CGI.unescape(params["payload"]))
+
+  if commit_json["ref"] != "refs/heads/master"
+    puts "Got notification of a non-master commit (#{commit_json["ref"]}) - ignoring."
+    return
+  end
+
+  commit_details = []
+  authors = []
+
+  commit_json["commits"].each do |commit|
+    authors << commit["author"]["name"]
+    api_url = "http://github.com/api/v2/json/commits/show/#{repo}/#{commit['id']}"
+    details = JSON.parse(`curl #{api_url}`)["commit"]
+    details["diff_url"] = commit["url"]
+    commit_details << details
+  end
+
+  Pony.mail(
+    :to => list_address,
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.gmail.com',
+      :port => '587',
+      :enable_starttls_auto => true,
+      :user_name => gmail_user_name,
+      :password => gmail_password,
+      :authentication => :plain,
+      :domain => "HELO" # why?
+    },
+    :subject => "#{authors.uniq.join(', ')} committed to master at #{repo}",
+    :body => erb(:email, :locals => {:commit_details => commit_details})
+  )
+=end
+
+end
+
+get '/up' do
+  "<html><head/><body>Yup.</body></html>"
+end
+
